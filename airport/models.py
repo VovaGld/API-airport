a7a08f9483691673bc9e98b3ffa298053805ac97
Vova <vova.gld05@gmail.com>
2025-02-21 00:06:20 +0200

    create models for crew & flight

--- airport/models.py
+++ airport/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db import models
+
 
 
 class Airport(models.Model):
@@ -51,3 +53,31 @@ class Airplane(models.Model):
     @property
     def capacity(self):
         return self.rows * self.seats_in_row
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        Airplane,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    departure_date = models.DateTimeField()
+    arrival_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.airplane.name} ({self.route.source} - {self.route.destination})"
+
