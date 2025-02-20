2b69c51514414a5c82839d8ec2c2631e7ba6a49d
Vova <vova.gld05@gmail.com>
2025-02-20 23:51:54 +0200

    create models for airpalne & airplane type

--- airport/models.py
+++ airport/models.py
@@ -27,3 +27,27 @@ class Route(models.Model):
 
     def __str__(self):
         return f"{self.source.closest_big_city} - {self.destination.closest_big_city}"
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType,
+        on_delete=models.CASCADE,
+        related_name="airplanes"
+    )
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
