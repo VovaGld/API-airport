c88be277ff348da6241f72ad33caf58ffba8fe23
Vova <vova.gld05@gmail.com>
2025-02-20 21:50:57 +0200

    create models for airpot & route

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ["name", "closest_big_city"]
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source.closest_big_city} - {self.destination.closest_big_city}"
